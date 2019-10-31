2718e4a1ef0e0ebf4f31ce42ecfe13e53ab82434
majiancheng <majiancheng@davdian.com>
2019-10-31 22:37:31 +0800

    [其他]说明：用户消息中心功能调整 [影响]消息中心功能调整

---
+++ src/main/java/com/coolplay/user/user/api/user/UserMessageController.java
@@ -0,0 +1,88 @@
+package com.coolplay.user.user.api.user;
+
+import com.coolplay.user.common.utils.PageConvertUtil;
+import com.coolplay.user.common.utils.ResponseUtil;
+import com.coolplay.user.common.utils.Result;
+import com.coolplay.user.security.utils.SecurityUtil;
+import com.coolplay.user.user.model.MessageModel;
+import com.coolplay.user.user.service.IMessageService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by majiancheng on 2019/10/31.
+ */
+@Controller
+@RequestMapping("/api/user/message")
+public class UserMessageController {
+
+    @Autowired
+    private IMessageService messageService;
+
+    /**
+     * 消息列表
+     *
+     * @param messageModel
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/list", method = RequestMethod.POST)
+    public Result list(@RequestBody MessageModel messageModel) {
+        messageModel.initPageInfo();
+        messageModel.setUserId(SecurityUtil.getCurrentUserId());
+        messageModel.setSort_("c_time_desc");
+
+        PageInfo<MessageModel> pageInfo = messageService
+                .selectByFilterAndPage(messageModel, messageModel.getPageNum(), messageModel.getPageSize());
+
+        return ResponseUtil.success(PageConvertUtil.grid(pageInfo));
+    }
+
+    /**
+     * 处理消息
+     *
+     * @param id 消息ID
+     * @param isAgree 是否同意（1：同意，2：不同意）
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/dealMessage", method = RequestMethod.POST)
+    public Result dealMessage(@RequestParam("id") Integer id, @RequestParam("isAgree") Integer isAgree) {
+        MessageModel messageModel = messageService.findById(id);
+
+        if(messageModel.getUserId() != SecurityUtil.getCurrentUserId()) {
+            return ResponseUtil.error("处理消息失败, 用户无权限操作其他消息.");
+        }
+
+        MessageModel updateMessage = new MessageModel();
+        updateMessage.setId(id);
+        updateMessage.setIsAgree(isAgree);
+        updateMessage.setIsRead(1);
+        int updateCnt = messageService.updateNotNull(updateMessage);
+
+        return ResponseUtil.success();
+    }
+
+    /**
+     * 消息详情信息
+     *
+     * @param id
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/detail", method = RequestMethod.POST)
+    public Result detail(@RequestParam("id") Integer id) {
+        MessageModel updateMessage = new MessageModel();
+        updateMessage.setId(id);
+        updateMessage.setIsRead(1);
+        int updateCnt = messageService.updateNotNull(updateMessage);
+
+        MessageModel messageModel = messageService.findById(id);
+
+        return ResponseUtil.success(messageModel);
+    }
+
+
+}
---
+++ src/main/java/com/coolplay/user/user/service/impl/MessageServiceImpl.java
@@ -0,0 +1,76 @@
+/*
+ * 北京果敢时代科技有限公司
+ * 北京市朝阳区望京SOHO T3 B座1607
+ * 邮编：100022
+ * 网址：www.davdian.com
+ */
+
+package com.coolplay.user.user.service.impl;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.coolplay.user.common.baseservice.impl.BaseService;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.coolplay.user.user.model.MessageModel;
+import tk.mybatis.mapper.entity.Example;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.*;
+import com.coolplay.user.user.dao.*;
+import com.coolplay.user.user.service.*;
+
+/**
+ * @author  davdian
+ * @version 1.0
+ * @since 1.0
+ */
+
+@Service("messageService")
+public class MessageServiceImpl extends BaseService<MessageModel> implements IMessageService{
+	@Autowired
+	private MessageMapper messageMapper;
+	
+	@Override
+	public MessageModel findById(Integer id) {
+		if(id == null) {
+			return null;
+		}
+		return messageMapper.findById(id);
+	}
+
+
+	public List<MessageModel> find(Map<String, Object> param) {
+		return messageMapper.find(param);
+	}
+
+	@Override
+	public PageInfo<MessageModel> selectByFilterAndPage(MessageModel messageModel, int pageNum,
+		int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		List<MessageModel> list = this.selectByFilter(messageModel);
+		return new PageInfo<>(list);
+	}
+
+	@Override
+	public List<MessageModel> selectByFilter(MessageModel messageModel) {
+		Example example = new Example(MessageModel.class);
+		Example.Criteria criteria = example.createCriteria();
+
+		if(messageModel.getUserId() != null) {
+			criteria.andEqualTo("userId", messageModel.getUserId());
+		}
+
+		if(StringUtils.isNotEmpty(messageModel.getSortWithOutOrderBy())) {
+			example.setOrderByClause(messageModel.getSortWithOutOrderBy());
+		}
+		return getMapper().selectByExample(example);
+	}
+}
