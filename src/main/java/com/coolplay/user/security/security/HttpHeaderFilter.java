9ab7ed4d0f8c712285043eb0ba4d55a715750e7a
majiancheng <majiancheng@davdian.com>
2019-12-15 20:31:05 +0800

    [其他]说明：update [影响]update

--- src/main/java/com/coolplay/user/security/security/HttpHeaderFilter.java
+++ src/main/java/com/coolplay/user/security/security/HttpHeaderFilter.java
@@ -1,12 +1,15 @@
 package com.coolplay.user.security.security;
 
 import com.alibaba.fastjson.JSON;
+import com.coolplay.user.common.utils.DateStyle;
+import com.coolplay.user.common.utils.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.util.Date;
 
 public class HttpHeaderFilter implements Filter {
 
@@ -20,7 +23,9 @@ public class HttpHeaderFilter implements Filter {
         //response.setHeader("Access-Control-Allow-Headers",
         //"Origin, X-Requested-With, Content-Type, Accept, " + tokenHeader);
 
-        System.out.println(String.format("前端请求参数:%s.", JSON.toJSONString(req.getParameterMap())));
+        System.out.println(String.format("前端 请求时间:%s, 请求URL:%s, 请求参数:%s.",
+                DateUtil.DateToString(new Date(), DateStyle.YYYY_MM_DD_HH_MM_SS), req.getRemoteAddr(),
+                JSON.toJSONString(req.getParameterMap())));
         chain.doFilter(req, res);
     }
 
