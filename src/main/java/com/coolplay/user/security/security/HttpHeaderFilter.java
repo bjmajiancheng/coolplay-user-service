0b5c4596591edd1a8fece991f749cdc5335b8979
majiancheng <majiancheng@davdian.com>
2019-12-15 20:53:53 +0800

    [其他]说明：u [影响]u

--- src/main/java/com/coolplay/user/security/security/HttpHeaderFilter.java
+++ src/main/java/com/coolplay/user/security/security/HttpHeaderFilter.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.Date;
 
@@ -23,9 +24,11 @@ public class HttpHeaderFilter implements Filter {
         //response.setHeader("Access-Control-Allow-Headers",
         //"Origin, X-Requested-With, Content-Type, Accept, " + tokenHeader);
 
-        System.out.println(String.format("前端 请求时间:%s, 请求参数:%s.",
-                DateUtil.DateToString(new Date(), DateStyle.YYYY_MM_DD_HH_MM_SS),
-                JSON.toJSONString(req.getParameterMap())));
+        HttpServletRequest httpRequest = (HttpServletRequest) req;
+
+        System.out.println(String.format("前端 请求时间:%s, 请求链接:%s, 请求参数:%s.",
+                DateUtil.DateToString(new Date(), DateStyle.YYYY_MM_DD_HH_MM_SS), httpRequest.getRequestURL(),
+                JSON.toJSONString(httpRequest.getParameterMap())));
         chain.doFilter(req, res);
     }
 
