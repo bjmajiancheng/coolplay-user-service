c06c32286fda4269a19b7bc214a6620b86788bfe
majiancheng <majiancheng@davdian.com>
2020-01-03 16:40:30 +0800

    [其他]说明：u [影响]u

--- src/main/java/com/coolplay/user/common/api/CommonController.java
+++ src/main/java/com/coolplay/user/common/api/CommonController.java
@@ -268,6 +268,7 @@ public class CommonController {
                     redisCache.set(String.format(SecurityConstant.WEATHER_DATA_KEY, lat, lon), weatherData, 12 * 60 * 60);
                 } else {
                     logger.info("获取天气数据异常, 返回结果:[{}].", content);
+                    System.out.println("获取天气数据异常, 返回结果:" + content);
                     return ResponseUtil.error("获取天气数据异常, 请稍后重试。");
                 }
             }
